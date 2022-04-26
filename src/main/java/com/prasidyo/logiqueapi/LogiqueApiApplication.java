f11732fed51573108d3053a22b5a053e5e53f028
prasidyo <prasidyo@diginovasi.com>
2022-04-26 19:04:24 +0700

    Initial commit

---
+++ src/main/java/com/prasidyo/logiqueapi/LogiqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.prasidyo.logiqueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiqueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasidyo/logiqueapi/LogiqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasidyo.logiqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
